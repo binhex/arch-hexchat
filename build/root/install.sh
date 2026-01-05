0f724a0e7959d255d583f11d361a7be1e9040f5c
binhex <megalith01@gmail.com>
2026-01-05 18:14:49 +0000

    try removal of gtk-doc

--- build/root/install.sh
+++ build/root/install.sh
@@ -43,7 +43,7 @@ refresh.sh
 source upd.sh
 
 # define pacman packages
-pacman_packages="lua perl thunar xarchiver geany gtk-doc gtk-doc-tools"
+pacman_packages="lua perl thunar xarchiver geany"
 
 # install compiled packages using pacman
 if [[ -n "${pacman_packages}" ]]; then
